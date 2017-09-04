2e4db55cfaa9064ef349c7501245e816e2d4a3e8
Jon Richardson <jon@robotmonster.ca>
2017-09-04 19:25:26 -0400

    Add example of first simple route

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom'; 
+import SimpleComponent from './simple/simple.component';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <Router>
+        <div>
+          <h2>A React Router Example</h2>
+
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link to="/simple">
+                  A Simple Link
+                </Link>
+              </li>
+            </ul>
+          </nav>
+
+          <Route path="/simple" component={SimpleComponent}/>
+          
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      </Router>
     );
   }
 }
---
+++ src/simple/simple.component.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SimpleComponent extends Component {
+    render() {
+        return (
+            <div>
+                This is the simple page.
+            </div>
+        );
+    }
+}
+
+export default SimpleComponent;
